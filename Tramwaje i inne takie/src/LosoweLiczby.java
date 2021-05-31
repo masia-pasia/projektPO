095901656877d7147d7595a6acb8b78256db5bf2
D-Woleek <84938240+D-Woleek@users.noreply.github.com>
2021-05-31 23:31:22 +0200

    Delete Tramwaje i inne takie directory

--- Tramwaje i inne takie/src/LosoweLiczby.java
+++
@@ -1,3 +0,0 @@
-public interface LosoweLiczby {
-    public int random();
-}
--- Tramwaje i inne takie/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args){
-        Tramwaj dwojka = new Tramwaj(2, 23, 8);
-        System.out.println(dwojka.random());
-    }
-}
